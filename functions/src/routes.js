3f56accd78e8c5cd85612d8b4e5f904fe84482ae
Vadim Batin <vadim.batin@extendaretail.com>
2020-11-22 22:20:14 +0200

    Refacor

--- functions/src/routes.js
+++ functions/src/routes.js
@@ -2,18 +2,15 @@ const express = require('express');
 
 const { requestVacation } = require('./modules/vacations');
 
-const { validationErrorHandler, errorHandler } = require('./modules/errors');
+const { validationErrorHandler } = require('./modules/errors');
 
 const router = express.Router();
-
-const noContent = (res, status = 204) => res.status(status).end();
 
 router.post('/vacations', (req, res) => {
     const form = req.body;
     return requestVacation(form)
-        .then(() => noContent(res))
-        .catch((error) => validationErrorHandler(error, req, res))
-        .catch((error) => errorHandler(error, req, res));
+        .then(() => res.status(200).end())
+        .catch((error) => validationErrorHandler(error, req, res));
 });
 
 module.exports = router;
